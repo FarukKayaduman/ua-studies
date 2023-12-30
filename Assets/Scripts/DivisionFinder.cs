e6c112714fbabbea22de94a69ec0e988702a199d
Faruk <farukkayaduman@outlook.com>
2023-12-30 18:59:52 +0300

    feat: Exact division study
    Week 2 project

---
+++ Assets/Scripts/DivisionFinder.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class DivisionFinder : MonoBehaviour
+{
+    private void Start()
+    {
+        // Modify the range.
+        FindDivisions(6, 77);
+    }
+
+    private static void FindDivisions(int startInclusive, int endInclusive)
+    {
+        FindDivisionsByNumber(startInclusive, endInclusive, 2);
+        FindDivisionsByNumber(startInclusive, endInclusive, 3);
+        FindDivisionsByNumber(startInclusive, endInclusive, 4);
+        FindDivisionsByNumber(startInclusive, endInclusive, 5);
+    }
+    
+    private static void FindDivisionsByNumber(int startInclusive, int endInclusive, int number)
+    {
+        string results = $"Divide by {number}: ";
+
+        for (int i = startInclusive; i <= endInclusive; i++)
+        {
+            if (i % number == 0)
+                results += $" {i}";
+        }
+
+        Debug.Log(results);
+    }
+}
